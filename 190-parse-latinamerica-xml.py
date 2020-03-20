81d051bfb5ca05186a1a87d121bfe6766e6578b8
Paul Wilson <paul.wilson@suez.com>
2020-03-20 17:20:27 +1100

    190

---
+++ 190-parse-latinamerica-xml.py
@@ -0,0 +1,27 @@
+import os
+from pathlib import Path
+from urllib.request import urlretrieve
+from xml.dom import minidom
+
+# import the countries xml file
+tmp = Path(os.getenv("TMP", "/tmp"))
+countries = tmp / 'countries.xml'
+
+if not countries.exists():
+    urlretrieve(
+        'https://bites-data.s3.us-east-2.amazonaws.com/countries.xml',
+        countries
+    )
+
+
+def get_income_distribution(xml=countries):
+    """
+    - Read in the countries xml as stored in countries variable.
+    - Parse the XML
+    - Return a dict of:
+      - keys = incomes (wb:incomeLevel)
+      - values = list of country names (wb:name)
+    """
+    pass
+
+c = minidom.parse(countries)
